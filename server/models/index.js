14eedf539404a1a5e8a7c3a033b73ffe79657e4e
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 13:52:31 +0200

    server(fix): include DB Host in ENV variables

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 
   pool: {
