da2fc4dd61ceec880d2b68656a457d878c291ed3
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 22:23:43 +0200

    server: modify cors options to allow all & credentials

--- server/index.js
+++ server/index.js
@@ -7,11 +7,15 @@ const router = require('./router');
 const cors = require('cors')
 
 app.use(cors({
-  origin: 'https://guidesmith-client.herokuapp.com/'
+  // origin: 'https://guidesmith-client.herokuapp.com/'
+  origin: "*",
+  methods: ['GET', 'PUT', 'DELETE', 'POST'],
+  credentials: true,
+  // preflightContinue: true,
 }));
 app.use(express.json());
 app.use(router);
 
-// app.listen(() => {
-//   console.log(`Example app listening at ${process.env.SV_URL}🚀`)
-// })
+app.listen(() => {
+  console.log(`Example app listening at ${process.env.SV_URL}🚀`)
+})
