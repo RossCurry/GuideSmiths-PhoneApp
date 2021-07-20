a69bdfbe5c9197f724f27ef09345ebaf3188e670
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 19:12:07 +0200

    server(fix) test server call with mock data

--- server/router.js
+++ server/router.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
-const { getPhoneData, addPhoneData, updatePhoneData, deletePhoneData } = require('./controller/phoneControllers');
-// const { mockData } = require('./mockData/seedData.json');
+const { addPhoneData, updatePhoneData, deletePhoneData } = require('./controller/phoneControllers');
+// const { getPhoneData, addPhoneData, updatePhoneData, deletePhoneData } = require('./controller/phoneControllers');
+const { mockData } = require('./mockData/seedData.json');
 
-router.get('/', getPhoneData);
+// router.get('/', getPhoneData);
+router.get('/', sendMockData);
 router.post('/mobile/create', addPhoneData);
 router.put('/mobile/update/:id', updatePhoneData);
 router.delete('/mobile/delete/:id', deletePhoneData);
 
-// const sendMockData = (req, res, next) => {
-//   res.send(mockData);
-//   next();
-// }
+const sendMockData = (req, res, next) => {
+  res.send(mockData);
+  next();
+}
 
 
 module.exports = router;
