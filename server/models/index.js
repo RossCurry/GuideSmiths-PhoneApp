186485e3310540e350054694ea1b9502e2e58469
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 19:29:26 +0200

    server(fix) change host name to database URL

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
-  host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   dialect: 'postgres',
 
   pool: {
