f7b16645e269f2c51db0a6bf5496bf2706a8b10d
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 19:21:46 +0200

    server(fix) try and send mock data

--- server/router.js
+++ server/router.js
@@ -3,16 +3,17 @@ const { addPhoneData, updatePhoneData, deletePhoneData } = require('./controller
 // const { getPhoneData, addPhoneData, updatePhoneData, deletePhoneData } = require('./controller/phoneControllers');
 const { mockData } = require('./mockData/seedData.json');
 
+const sendMockData = (req, res, next) => {
+  res.send(mockData);
+  next();
+}
+
 // router.get('/', getPhoneData);
 router.get('/', sendMockData);
 router.post('/mobile/create', addPhoneData);
 router.put('/mobile/update/:id', updatePhoneData);
 router.delete('/mobile/delete/:id', deletePhoneData);
 
-const sendMockData = (req, res, next) => {
-  res.send(mockData);
-  next();
-}
 
 
 module.exports = router;
