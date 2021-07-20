d4cc3dc830b9cd55e47a66ba676f43a1ad9b5e8d
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-20 20:43:34 +0200

    server: add ssl true to sequelize config

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect:  'postgres',
+  ssl: true
 })
   
 // const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
